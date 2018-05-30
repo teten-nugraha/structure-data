3095052266cbbf50ec6c379702880d611e72640e
teten <teten.nugraha18@gmail.com>
2018-05-30 21:34:21 +0700

    first commit

---
+++ src/structuredata/o1/Vectore/CobaVector.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package structuredata.o1.Vectore;
+
+/**
+ *
+ * @author Teten Nugraha
+ */
+public class CobaVector {
+    
+}
