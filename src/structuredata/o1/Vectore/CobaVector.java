36a9bacbc02f687041b25255c390ab4ab59fb100
teten <teten.nugraha18@gmail.com>
2018-05-30 21:46:08 +0700

    vector

--- src/structuredata/o1/Vectore/CobaVector.java
+++ src/structuredata/o1/Vectore/CobaVector.java
@@ -5,10 +5,48 @@
  */
 package structuredata.o1.Vectore;
 
+import java.util.Vector;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Teten Nugraha
  */
 public class CobaVector {
+    
+    static int ukuranVector;
+    static Vector<Integer> vector;
+    
+    public static void main(String[] args) {
+        System.out.print("Berapa ukuran Vector ? ");
+        ukuranVector = inputData();
+        buatVector();
+        bacaData();
+        tulisData();
+    }
+    
+    private static int inputData() {
+        int data = Integer.valueOf(JOptionPane.showInputDialog(null,"Berapa nilai vector ?"));
+        return data;
+    }
+    
+    private static void buatVector() {
+        vector = new Vector<Integer>(ukuranVector);
+    }
+    
+    private static void bacaData() {
+        
+        for (int i = 0; i < ukuranVector; i++) {
+            int data = Integer.valueOf(JOptionPane.showInputDialog(null,"Berapa nilai vector  ke "+(i+1)+"?"));
+            vector.add(data);
+        }
+        
+    }
+    
+    private static void tulisData() {
+        for (int i = ukuranVector-1; i >= 0; i--) {
+            System.out.println("Data ke +"+(i+1)+" = "+vector.get(i));
+        }
+    }
     
 }
